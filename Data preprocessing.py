dbce6e7395bb03e4aba1c3e4b1833d5111a8b8f5
Greg Pye <greg.pye@gmail.com>
2021-07-25 17:13:52 +0100

    added blak toyear regex

--- Data preprocessing.py
+++ Data preprocessing.py
@@ -7,6 +7,7 @@ import json
 from pathlib import Path
 import sys
 import time
+import re
 
 # Load data into dataframe
 data_folder = Path("C:/Users/gregp/Documents/kaggle/input/imdb-review-dataset/")
@@ -15,6 +16,12 @@ data_folder = Path("C:/Users/gregp/Documents/kaggle/input/imdb-review-dataset/")
 files_to_load = ["sample.json"]
 reviews_json = list()
 
+# Define the regex to use for year matching
+# Base on starting with a space (='\s') 19 or 20, then two digits
+# regex compiled as it will be used many times over
+year_pattern_regex = re.compile(r"\s(19|20)\d{2}")
+# 
+
 startTime = time.time()
 for current_file in files_to_load:
     print(f"Starting load of {current_file}...")
@@ -22,6 +29,12 @@ for current_file in files_to_load:
     with open(file_to_read, mode='r') as file:
         new_reviews = json.load(file)
         for review in new_reviews:
+            # Add a column with the year
+            review['review_year'] = review['review_date'][-4:]
+
+            # Replace any occurrances of a year in the 'review_detail' with QQQQ
+            year_pattern_regex.sub(' QQQQ',review['review_detail'])
+
             reviews_json.append(review)
     print(f"Finished load of {current_file} at {time.time() - startTime:.2f} total seconds elapsed")
 print(f"Loading complete after {time.time() - startTime:.2f} seconds, {len(reviews_json):,} items in reviews_json")
@@ -29,3 +42,4 @@ print(f"Loading complete after {time.time() - startTime:.2f} seconds, {len(revie
 reviews_total_count = len(reviews_json)
 print(f"Total reviews in the working data: {reviews_total_count:,}")
 
+print(f"example output {reviews_json[0]}")
