171f3df479424ef1c17403173fa7fd3270d351b1
Greg Pye <greg.pye@gmail.com>
2021-07-26 21:13:31 +0100

    Added title replace with TTTT

--- Data preprocessing.py
+++ Data preprocessing.py
@@ -1,11 +1,9 @@
-# Alternate pre-processing trial
+# Data pre-processior
 #
 # Created by Greg on 24/07/21
 
-import pandas as pd
 import json
 from pathlib import Path
-import sys
 import time
 import re
 import zlib # used for a simple hash
@@ -13,8 +11,8 @@ import zlib # used for a simple hash
 # Define data source and target
 data_source_folder = Path("C:/Users/gregp/Documents/kaggle/imdb-review-dataset/raw")
 # Enumerate the failenames rather than reading the directory, so outcome is fully predictable
-files_to_load = ["part-01.json", "part-02.json", "part-03.json", "part-04.json", "part-05.json", "part-06.json"]
-# files_to_load = ["sample.json"]
+# files_to_load = ["part-01.json", "part-02.json", "part-03.json", "part-04.json", "part-05.json", "part-06.json"]
+files_to_load = ["sample.json"]
 
 data_target_folder = Path("C:/Users/gregp/Documents/kaggle/imdb-review-dataset/pre_processed")
 data_target_filename_prefix = "pre_processed_group_"
@@ -24,7 +22,15 @@ target_files = {}
 # Define the regex to use for year matching
 # Base on starting with 19 or 20, then two digits
 # regex compiled as it will be used many times over
+# Note that there is a unicode en-dash that looks confusingly like it has got left over 
+# It appers as \u2013 - see https://www.fileformat.info/info/unicode/char/2013/index.htm 
 year_pattern_regex = re.compile(r"(19|20)\d{2}")
+
+# Define the regex to use in matching film title
+# Splits string into two search blocks, with the first being the title
+# The second is a whitespace followed by an open bracket followed by four digits, then any other characters (which may be none), then a closing bracket
+# Matching the four digits ensures a year follows the bracket
+title_pattern_regex = re.compile(r"(.*)(\s\(\d{4}.*\))")
 
 # Notes on preprocessing
 # 1) For simplicity later on, add a column with the target review year
@@ -68,7 +74,16 @@ for current_file in files_to_load:
             # Replace any occurrances of a year in the 'review_summary' and'review_detail' fields with QQQQ
             review['review_summary'] = year_pattern_regex.sub('QQQQ',review['review_summary'])
             review['review_detail'] = year_pattern_regex.sub('QQQQ',review['review_detail'])
-            review['movie'] = year_pattern_regex.sub('QQQQ',review['movie'])
+            review['movie_hidden_years'] = year_pattern_regex.sub('QQQQ',review['movie'])
+
+            # Replace any occurances of the movie title with TTTT
+            title_search = title_pattern_regex.match(review['movie']).group(1)
+            # Use a regex sub function to ensure case insensitivity
+            # Effctively like below but case insensitive
+            # review['review_summary'] = review['review_summary'].replace(title_search, "TTTT")
+            specific_title_regex = re.compile(re.escape(title_search), re.IGNORECASE)
+            review['review_summary'] = specific_title_regex.sub('TTTT',review['review_summary'])
+            review['review_detail'] = specific_title_regex.sub('TTTT',review['review_detail'])
 
             # Write to correct sub-group
             target_files[sub_group].write(json.dumps(review))
