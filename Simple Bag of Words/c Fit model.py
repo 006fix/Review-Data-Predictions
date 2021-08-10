7d040bbc6c004fd1cff4c7bc851473b6f27fe57d
Greg Pye <greg.pye@gmail.com>
2021-08-10 20:21:59 +0100

    Tweaked test set accuracy and loss metrics

--- Simple Bag of Words/c Fit model.py
+++ Simple Bag of Words/c Fit model.py
@@ -15,10 +15,6 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, BatchNormalization
 import matplotlib.pyplot as plt
 
-# Define data source and target
-data_source_folder = Path("C:/Users/gregp/Documents/kaggle/imdb-review-dataset/simple_BOW")
-files_to_load = ["BOW_training_data.txt"]
-
 startTime = time.time()
 
 # Load dictonary source
@@ -26,6 +22,10 @@ tokenized_dictionary = Path("C:/Users/gregp/Documents/kaggle/imdb-review-dataset
 with open(tokenized_dictionary) as f:
     tokenizer = tokenizer_from_json(f.read())
 print(f"Loaded tokenized dictionary based on {tokenizer.document_count} reviews, and including {len(tokenizer.word_index)} words")
+
+# Define training data source
+data_source_folder = Path("C:/Users/gregp/Documents/kaggle/imdb-review-dataset/simple_BOW")
+training_files_to_load = ["BOW_training_data.txt"]
 
 # Define the target
 model_save_file = Path("C:/Users/gregp/Documents/kaggle/imdb-review-dataset/simple_BOW/saved_model.tf")
@@ -35,7 +35,7 @@ training_texts = []
 training_outcomes = []
 process_count = 0
 # non_number_count = 0
-for current_file in files_to_load:
+for current_file in training_files_to_load:
     print(f"Starting load of training data by {current_file}...")
     file_to_read = data_source_folder / current_file
     with open(file_to_read, mode='r') as file:        
@@ -66,6 +66,32 @@ for current_file in files_to_load:
 print(f"{process_count} items of training data loaded after {time.time() - startTime:.2f} seconds")
 # print(f"Found {non_number_count} ratings that have a non string rating")
 
+# Define test data source
+data_source_folder = Path("C:/Users/gregp/Documents/kaggle/imdb-review-dataset/simple_BOW")
+test_files_to_load = ["BOW_test_data.txt"]
+
+# Load the training data
+test_texts = []
+test_outcomes = []
+process_count = 0
+# non_number_count = 0
+for current_file in test_files_to_load:
+    print(f"Starting load of training data by {current_file}...")
+    file_to_read = data_source_folder / current_file
+    with open(file_to_read, mode='r') as file:        
+        for line in file:
+            review = json.loads(line.strip())
+           
+            # Load the test texts
+            test_texts.append (review['review_detail'])
+
+            # Load the outcomes
+            test_outcomes.append (review['bin_id'])
+
+            process_count += 1
+
+print(f"{process_count} items of test data loaded after {time.time() - startTime:.2f} seconds")
+
 # Set the desired dictionary size
 max_words = 1500
 tokenizer.num_words = max_words
@@ -78,14 +104,28 @@ print(f"Tokenizing training texts complete after {time.time() - startTime:.2f} s
 print(f"Training texts shape is {Xtrain.shape}")
 print(f"Training texts example is {Xtrain[0]}")
 
-# Buld the target matrix
+# Buld the target training matrix
 # Target is an ordinal variable -
 # But, for initial version treat as a classification
 # Uses the Keras to_categorical model
-# TODO Use modified model e.g. sequential ones, not just 'hot one'
 training_outcomes = array(training_outcomes)
 Ytrain = to_categorical(training_outcomes)
 print(f"Training outcomes shape is {Ytrain.shape}")
+
+# Build the tokenized test texts
+print(f"Tokenizing test texts, using dictionary of {max_words}")
+Xtest = tokenizer.texts_to_matrix(test_texts, mode='freq')
+print(f"Tokenizing test texts complete after {time.time() - startTime:.2f} seconds")
+print(f"Test texts shape is {Xtest.shape}")
+print(f"Test texts example is {Xtest[0]}")
+
+# Buld the target test matrix
+# Target is an ordinal variable -
+# But, for initial version treat as a classification
+# Uses the Keras to_categorical model
+test_outcomes = array(test_outcomes)
+Ytest = to_categorical(test_outcomes)
+print(f"Test outcomes shape is {Ytest.shape}")
 
 # create model
 print(f"{time.time() - startTime:.2f} : Creating model")
@@ -104,6 +144,13 @@ print(f"{time.time() - startTime:.2f} : Model compiled")
 history = model.fit(Xtrain, Ytrain, validation_split=0.2, epochs=35, verbose=2)
 print(f"{time.time() - startTime:.2f} : Model fitted")
 
+# Evaluate the model on the test data
+loss, acc = model.evaluate(Xtest, Ytest, verbose=2)
+print("Checking against test set")
+print('Test Accuracy: %f' % (acc))
+print('Test Loss: %f' % (loss))
+
+# Show metrics - model build progress
 plt.plot (history.history['accuracy'])
 plt.plot (history.history['val_accuracy'])
 plt.title('model accuracy')
